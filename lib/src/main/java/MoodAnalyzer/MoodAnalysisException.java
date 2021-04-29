c8a01215b037dd3749519c1bf1cfdf95c482d775
mohdarshad1 <mdarshad181@gmail.com>
2021-04-29 17:38:16 +0530

    [Mohd Arshad] : Add UC3 Throw Exception

---
+++ lib/src/main/java/MoodAnalyzer/MoodAnalysisException.java
@@ -0,0 +1,17 @@
+package MoodAnalyzer;
+
+public class MoodAnalysisException extends Exception {
+
+	enum MyException_Type
+    {
+        EMPTY,NULL;
+    }
+    MyException_Type type;
+    MyException_Type exceptionMessage;
+    MoodAnalysisException(MyException_Type type,String message)
+    {
+        super(message);
+        this.type=type;
+        System.out.println(message);
+    }
+}
